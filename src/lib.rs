a3443c9cc85fd3d6e667e6319f1ddecb5dd5f978
--global <bruno.glowaski@outlook.com>
2024-09-06 23:30:14 -0400

    Identifier parser

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,29 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), 
     }
 }
 
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    // First character needs to be alphabetic
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    // The next characters need to be either alphanumeric or a dash.
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
 #[test]
 fn a_parser() {
     assert_eq!(the_letter_a("a"), Ok(("", ())));
@@ -33,7 +56,22 @@ fn a_parser() {
 #[test]
 fn literal_parser() {
     let parse_joe = match_literal("Joe");
-    assert_eq!(parse_joe("Joe"), Ok(("", ())),);
-    assert_eq!(parse_joe("Joe! Joseph!"), Ok(("! Joseph!", ())),);
-    assert_eq!(parse_joe("Robert"), Err("Robert"),);
+    assert_eq!(parse_joe("Joe"), Ok(("", ())));
+    assert_eq!(parse_joe("Joe! Joseph!"), Ok(("! Joseph!", ())));
+    assert_eq!(parse_joe("Robert"), Err("Robert"));
+}
+
+#[test]
+fn identifier_parser() {
+    assert_eq!(
+        Ok(("", "i-am-an-identifier".to_string())),
+        identifier("i-am-an-identifier")
+    );
+    assert_eq!(
+        Ok((" entirely an identifier", "not".to_string())),
+        identifier("not entirely an identifier")
+    );
+    assert_eq!(Err("!not an identifier"), identifier("!not an identifier"));
+}
+
 }
