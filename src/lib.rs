890f610acf19a7aef78ee94efa2266656e13c6d0
--global <bruno.glowaski@outlook.com>
2024-09-06 23:30:14 -0400

    First parser

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
