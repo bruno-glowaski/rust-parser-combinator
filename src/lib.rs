f8ed6efa8cc428a235e895e6cb2f6f676ad14c22
--global <bruno.glowaski@outlook.com>
2024-09-06 23:30:14 -0400

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,10 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
         _ => Err(input),
     }
 }
+
+#[test]
+fn a_parser() {
+    assert_eq!(the_letter_a("a"), Ok(("",())));
+    assert_eq!(the_letter_a("abc"), Ok(("bc",())));
+    assert_eq!(the_letter_a("cba"), Err("cba"));
+}
