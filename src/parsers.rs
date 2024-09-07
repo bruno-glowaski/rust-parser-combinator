c53d6074f41257e26ff529a393edeec3b7ecc9f5
--global <bruno.glowaski@outlook.com>
2024-09-06 23:30:15 -0400

    Left and right combinators

--- src/parsers.rs
+++ src/parsers.rs
@@ -75,3 +75,19 @@ where
             .map(|(next, result)| (next, map_fn(result)))
     }
 }
+
+pub fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    map(pair(parser1, parser2), |(r, _)| r)
+}
+
+pub fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    map(pair(parser1, parser2), |(_, r)| r)
+}
