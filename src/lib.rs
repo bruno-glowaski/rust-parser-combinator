8bf66faeecffe746927ac16d48bf6e36c9f36693
--global <bruno.glowaski@outlook.com>
2024-09-06 23:30:15 -0400

    Add Parser and ParseResult

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,12 @@
+mod parsers;
+
+pub use parsers::*;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct Element {
     name: String,
     attributes: Vec<(String, String)>,
     children: Vec<Element>,
-}
-
-fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
-    match input.chars().next() {
-        Some('a') => Ok((&input['a'.len_utf8()..], ())),
-        _ => Err(input),
-    }
-}
-
-fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
-    // move |input: &str| match input.get(0..expected.len()) {
-    //     Some(next) if next == expected => Ok((&input[expected.len()..], ())),
-    //     _ => Err(input),
-    // }
-    move |input: &str| match input.strip_prefix(expected) {
-        Some(remainder) => Ok((remainder, ())),
-        _ => Err(input),
-    }
-}
-
-fn identifier(input: &str) -> Result<(&str, String), &str> {
-    let mut matched = String::new();
-    let mut chars = input.chars();
-
-    // First character needs to be alphabetic
-    match chars.next() {
-        Some(next) if next.is_alphabetic() => matched.push(next),
-        _ => return Err(input),
-    }
-
-    // The next characters need to be either alphanumeric or a dash.
-    while let Some(next) = chars.next() {
-        if next.is_alphanumeric() || next == '-' {
-            matched.push(next);
-        } else {
-            break;
-        }
-    }
-
-    let next_index = matched.len();
-    Ok((&input[next_index..], matched))
-}
-
-fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
-where
-    P1: Fn(&str) -> Result<(&str, R1), &str>,
-    P2: Fn(&str) -> Result<(&str, R2), &str>,
-{
-    // move |input: &str| match parser1(input) {
-    //     Ok((next_input, result1)) => match parser2(next_input) {
-    //         Ok((remainder, result2)) => Ok((remainder, (result1, result2))),
-    //         Err(err) => Err(err),
-    //     },
-    //     Err(err) => Err(err),
-    // }
-    move |input: &str| {
-        parser1(input).and_then(|(next, r1)| {
-            parser2(next).and_then(|(remainder, r2)| Ok((remainder, (r1, r2))))
-        })
-    }
-}
-
-fn map<P, F, A, B>(parser: P, map_fn: F) -> impl Fn(&str) -> Result<(&str, B), &str>
-where
-    P: Fn(&str) -> Result<(&str, A), &str>,
-    F: Fn(A) -> B,
-{
-    move |input: &str| match parser(input) {
-        Ok((next, result)) => Ok((next, map_fn(result))),
-        Err(err) => Err(err),
-    }
 }
 
 #[test]
@@ -86,9 +19,9 @@ fn a_parser() {
 #[test]
 fn literal_parser() {
     let parse_joe = match_literal("Joe");
-    assert_eq!(parse_joe("Joe"), Ok(("", ())));
-    assert_eq!(parse_joe("Joe! Joseph!"), Ok(("! Joseph!", ())));
-    assert_eq!(parse_joe("Robert"), Err("Robert"));
+    assert_eq!(parse_joe.parse("Joe"), Ok(("", ())));
+    assert_eq!(parse_joe.parse("Joe! Joseph!"), Ok(("! Joseph!", ())));
+    assert_eq!(parse_joe.parse("Robert"), Err("Robert"));
 }
 
 #[test]
@@ -109,8 +42,8 @@ fn pair_combinator() {
     let tag_opener = pair(match_literal("<"), identifier);
     assert_eq!(
         Ok(("/>", ((), "my-first-element".to_string()))),
-        tag_opener("<my-first-element/>")
+        tag_opener.parse("<my-first-element/>")
     );
-    assert_eq!(Err("oops"), tag_opener("oops"));
-    assert_eq!(Err("!oops"), tag_opener("<!oops"));
+    assert_eq!(Err("oops"), tag_opener.parse("oops"));
+    assert_eq!(Err("!oops"), tag_opener.parse("<!oops"));
 }
---
+++ src/parsers.rs
@@ -0,0 +1,77 @@
+pub type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
+pub trait Parser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+}
+
+impl<'a, F, Output> Parser<'a, Output> for F
+where
+    F: Fn(&'a str) -> ParseResult<'a, Output>,
+{
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+        self(input)
+    }
+}
+
+pub fn the_letter_a(input: &str) -> ParseResult<()> {
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
+
+pub fn identifier(input: &str) -> ParseResult<String> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    // First character needs to be alphabetic
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    // The next characters need to be either alphanumeric or a dash.
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
+pub fn match_literal<'a>(expected: &'static str) -> impl Parser<'a, ()> {
+    move |input: &'a str| match input.strip_prefix(expected) {
+        Some(remainder) => Ok((remainder, ())),
+        _ => Err(input),
+    }
+}
+
+pub fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    move |input| {
+        parser1.parse(input).and_then(|(next, r1)| {
+            parser2
+                .parse(next)
+                .map(|(remainder, r2)| (remainder, (r1, r2)))
+        })
+    }
+}
+
+pub fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
+where
+    P: Parser<'a, A>,
+    F: Fn(A) -> B,
+{
+    move |input| {
+        parser
+            .parse(input)
+            .map(|(next, result)| (next, map_fn(result)))
+    }
+}
